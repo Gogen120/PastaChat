67aa5ce948acf2667315eeb24a45d5c23549f111
matiek8 <matiek8@gmail.com>
2018-09-24 08:25:36 +0300

    client part
    client part

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Layout from './components/Layout';
+import './index.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Layout title="Chat app BAby"/>
+        );
+    }
+}
+
